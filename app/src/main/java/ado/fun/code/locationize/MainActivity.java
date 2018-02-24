8663d04cc839bff0ee54aaa30bfdcac5ffddf247
Shubham Singhal <shubham21197@gmail.com>
2018-02-24 14:55:30 +0530

    Location Listener

--- app/src/main/java/ado/fun/code/locationize/MainActivity.java
+++ app/src/main/java/ado/fun/code/locationize/MainActivity.java
@@ -1,17 +1,62 @@
 package ado.fun.code.locationize;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    LocationManager locationManager;
+    TextView locationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GPSTracker gpsTracker = new GPSTracker(this);
 
-        if (gpsTracker.getIsGPSTrackingEnabled())
-            String.valueOf(gpsTracker.latitude)
+        locationText=(TextView)findViewById(R.id.locText);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+
+        }
+
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    locationText.setText("Longitude: "+location.getLongitude() + "Latitude: " + location.getLatitude());
+                }
+                @Override
+                public void onProviderDisabled(String provider) {
+                    Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+                @Override
+                public void onProviderEnabled(String provider) {
+                    // TODO Auto-generated method stub
+                }
+                @Override
+                public void onStatusChanged(String provider, int status,
+                                            Bundle extras) {
+                    // TODO Auto-generated method stub
+                }
+            });
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
+
